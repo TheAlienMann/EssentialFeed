7be0d0befe82c299a97fcf8bce0c9b57d5b0d4cc
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-08-30 23:28:53 +0330

    moved  to EssentialFeedTests

---
+++ Tests/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -0,0 +1,11 @@
+import Foundation
+import XCTest
+
+class FeedEndpointTests: XCTestCase {
+    // func test_feed_endpointURL() {
+//     let baseURL = URL(string: "htt://base-url.com")!
+//     let received = FeedEndpoint.get().url(baseURL: baseURL)
+
+//     xctasser
+    // }
+}
