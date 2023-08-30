9f513f290f4bec57696d11ce213642fb5ab7272a
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-08-30 23:19:49 +0330

    fix typo

--- Sources/EssentialFeed/Feed Cache/FeedStore.swift
+++ Sources/EssentialFeed/Feed Cache/FeedStore.swift
@@ -4,6 +4,6 @@ public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
     func deleteCachedFeed() throws
-    func insert(_ feed: [LocalImageFeed], timestamp: Date) throws
+    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws
     func retrieve() throws -> CachedFeed?
 }
