49ebec599d33c51f3c7c5f0044cfc786a751158b
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-08-30 23:37:20 +0330

    add platform to package.swift

--- Tests/EssentialFeedTests/Feed API/FeedEndpointTests.swift
+++ Tests/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -1,11 +1,15 @@
+import EssentialFeed
 import Foundation
 import XCTest
 
 class FeedEndpointTests: XCTestCase {
-    // func test_feed_endpointURL() {
-//     let baseURL = URL(string: "htt://base-url.com")!
-//     let received = FeedEndpoint.get().url(baseURL: baseURL)
+    func test_feed_endpointURL() {
+        let baseURL = URL(string: "htt://base-url.com")!
+        let received = FeedEndpoint.get().url(baseURL: baseURL)
 
-//     xctasser
-    // }
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url", "host")
+        XCTAssertEqual(received.path, "/v1/feed", "path")
+        XCTAssertEqual(received.query, "limit=10", "query")
+    }
 }
